f70321f3709df377b96f2f1394ce438f138fd6d5
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2022-10-11 20:30:23 -0300

    Quick fix

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ slider.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(
 slider.addEventListener('input', () => {
     slider.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) ${(slider.value - slider.min)/(slider.max - slider.min)*100}%, hsl(224, 65%, 95%) ${(slider.value - slider.min)/(slider.max - slider.min)*100}%, hsl(224, 65%, 95%) 100%)`;
 })
+
+// https://github.com/awsmPuff/interactive-pricing-component
