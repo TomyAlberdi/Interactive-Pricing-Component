c341bf89854cdf8c7c37419e08b32fd9208f013b
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2022-10-11 20:27:23 -0300

    CSS Mobile

---
+++ script.js
@@ -0,0 +1,6 @@
+const slider = document.querySelector('input[type="range"]')
+slider.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) ${(slider.value - slider.min)/(slider.max - slider.min)*100}%, hsl(224, 65%, 95%) ${(slider.value - slider.min)/(slider.max - slider.min)*100}%, hsl(224, 65%, 95%) 100%)`;
+
+slider.addEventListener('input', () => {
+    slider.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) ${(slider.value - slider.min)/(slider.max - slider.min)*100}%, hsl(224, 65%, 95%) ${(slider.value - slider.min)/(slider.max - slider.min)*100}%, hsl(224, 65%, 95%) 100%)`;
+})
