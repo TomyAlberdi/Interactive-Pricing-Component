bdb0831f4f7493189a6bda10b848599793db5bb2
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2022-10-16 22:35:16 -0300

    Func

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
 // https://github.com/awsmPuff/interactive-pricing-component
+const changeValue = () => {
+    switch (slider.value) {
+        case "0":
+            views.innerHTML = "10K PAGEVIEWS"
+            if (check.checked) {
+                price.innerHTML = "$6"
+            } else {
+                price.innerHTML = "$8"
+            }
+            break;
+        case "1":
+            views.innerHTML = "50K PAGEVIEWS"
+            if (check.checked) {
+                price.innerHTML = "$9"
+            } else {
+                price.innerHTML = "$12"
+            }
+            break;
+        case "2":
+            views.innerHTML = "100K PAGEVIEWS"
+            if (check.checked) {
+                price.innerHTML = "$12"
+            } else {
+                price.innerHTML = "$16"
+            }
+            break;
+        case "3":
+            views.innerHTML = "500K PAGEVIEWS"
+            if (check.checked) {
+                price.innerHTML = "$18"
+            } else {
+                price.innerHTML = "$24"
+            }
+            break;
+        case "4":
+            views.innerHTML = "1M PAGEVIEWS"
+            if (check.checked) {
+                price.innerHTML = "$27"
+            } else {
+                price.innerHTML = "$36"
+            }
+            break;
+    }
+}
 // Slider color change
 const slider = document.querySelector('input[type="range"]')
 slider.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) ${(slider.value - slider.min)/(slider.max - slider.min)*100}%, hsl(224, 65%, 95%) ${(slider.value - slider.min)/(slider.max - slider.min)*100}%, hsl(224, 65%, 95%) 100%)`;
@@ -9,13 +53,20 @@ slider.addEventListener('input', () => {
 // Checkbox animation
 const ball = document.querySelector('.ball')
 const check = document.querySelector('input[type="checkbox"]')
-let ballChange = (c) => {
-    c.checked ? (ball.style.left = "55%", check.style.backgroundColor = "hsl(174, 86%, 45%)") : (ball.style.left = "5%", check.style.backgroundColor = "hsl(223, 50%, 87%)")
-}
+check.checked = false
 check.addEventListener('click', () => {
-    ballChange(check)
+    if (check.checked) {
+        ball.style.left = "55%"
+        check.style.backgroundColor = "hsl(174, 86%, 45%)"
+    } else {
+        ball.style.left = "5%"
+        check.style.backgroundColor = "hsl(223, 50%, 87%)"
+    }
+    changeValue()
 })
-ball.addEventListener('click', () => {
-    check.checked = !check.checked
-    ballChange(check)
+// Page func logic
+const views = document.querySelector('#page-views')
+const price = document.querySelector('.price h1')
+slider.addEventListener('input', () => {
+    changeValue()
 })
